8e97bc8a433bdc509a5bc0071325277d7c36b20c
brucelane <bruce@batchass.fr>
2017-05-06 21:50:31 +0200

    adding https://github.com/elijahmanor/spectacle-terminal

--- presentation/index.js
+++ presentation/index.js
@@ -30,7 +30,12 @@ import CodeSlide from "spectacle-code-slide";
 import createTheme from "spectacle/lib/themes/default";
 
 // Import custom component
-import Interactive from "../assets/interactive";
+//import Interactive from "../assets/interactive";
+import Terminal from "spectacle-terminal";
+import Typist from "react-typist";
+import Loading from "react-loading";
+
+const cursor = { show: true, blink: true, element: "|", hideWhenDone: true, hideWhenDoneDelay: 1000 };
 
 // Require CSS
 require("normalize.css");
@@ -38,14 +43,15 @@ require("spectacle/lib/themes/default/index.css");
 
 
 const images = {
-  vdlogo: require("../assets/videodromm-logo.jpg")
+  vdlogo: require("../assets/videodromm-logo.jpg"),
+  ws: require("../assets/websocket.jpg")
 };
 
 preloader(images);
 
 const theme = createTheme({
-  primary: "#857D41",
-  secondary: "#540804"
+  primary: "#220022",
+  secondary: "#c9ca00"
 });
 
 export default class Presentation extends React.Component {
@@ -53,14 +59,54 @@ export default class Presentation extends React.Component {
     return (
       <Spectacle theme={theme}>
         <Deck transition={["zoom", "slide"]} transitionDuration={500}>
-          <Slide transition={["zoom"]} bgColor="primary">
-            <Heading size={1} fit caps lineHeight={1} textColor="black">
+          <Slide transition={["zoom"]} bgColor="black">
+            <Heading size={1} fit caps lineHeight={1} >
               Videodromm
             </Heading>
-            <Heading size={1} fit caps>
+            <Heading size={1} fit caps textColor="secondary">
               Open source project
             </Heading>
+            <Image width="40%" src={images.vdlogo}/>
             <Text textSize="1.5em" margin="20px 0px 0px" bold>By Bruce LANE</Text>
+          </Slide>
+          <Slide transition={["zoom", "fade"]} bgColor="black">
+            <Heading caps fit>Created with</Heading>
+            <Layout>
+              <Fill>
+                <Heading size={4} caps textColor="secondary" bgColor="primary" margin={10}>
+                  Cinder
+                </Heading>
+              </Fill>
+              <Fill>
+                <Heading size={4} caps textColor="secondary" bgColor="primary" margin={10}>
+                  GLSL
+                </Heading>
+              </Fill>
+              <Fill>
+                <Heading size={4} caps textColor="secondary" bgColor="primary" margin={10}>
+                  NodeJS
+                </Heading>
+              </Fill>
+              <Fill>
+                <Heading size={4} caps textColor="secondary" bgColor="primary" margin={10}>
+                  ReactJS
+                </Heading>
+              </Fill>
+            </Layout>
+          </Slide>
+          <Slide transition={["spin", "slide"]} bgColor="primary">
+            <Heading size={4} caps fit textColor="tertiary"><code>Setup Netdromm</code></Heading>
+            <Terminal title="bruce@videodromm" output={ [
+              <Typist cursor={ cursor }>git clone https://github.com/videodromm/netdromm</Typist>,
+              <Typist cursor={ cursor }>npm install</Typist>,
+              <Typist cursor={ cursor }>npm start</Typist>,
+              <div>
+                <div style={{ color: "#33B969"}}>info: Application server running on port 8088!</div>
+                <div style={{ color: "#DEC612"}}>info: user connected: ::ffff:127.0.0.1</div>
+                <Image width="40%" src={images.ws}/>
+              </div>]}
+            />
+            
           </Slide>
         </Deck>
       </Spectacle>
