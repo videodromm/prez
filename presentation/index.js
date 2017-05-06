bd2754120fd407b65ef70545257ecc6511e9a83a
brucelane <bruce@batchass.fr>
2017-05-06 04:38:23 +0200

    webpack

---
+++ presentation/index.js
@@ -0,0 +1,73 @@
+// Import React
+import React from "react";
+
+// Import Spectacle Core tags
+import {
+  Appear,
+  BlockQuote,
+  Cite,
+  CodePane,
+  Deck,
+  Fill,
+  Heading,
+  Image,
+  Layout,
+  Link,
+  ListItem,
+  List,
+  Markdown,
+  Quote,
+  Slide,
+  Spectacle,
+  Text
+} from "spectacle";
+
+// Import image preloader util
+import preloader from "spectacle/lib/utils/preloader";
+// codeslide from https://github.com/thejameskyle/spectacle-code-slide
+import CodeSlide from "spectacle-code-slide";
+// Import theme
+import createTheme from "spectacle/lib/themes/default";
+
+// Import custom component
+import Interactive from "../assets/interactive";
+
+// Require CSS
+require("normalize.css");
+require("spectacle/lib/themes/default/index.css");
+
+
+const images = {
+  webgl: require("../assets/webgl.jpg"),
+  vdlogo: require("../assets/videodromm-logo.jpg"),
+  neo: require("../assets/neo.jpg"),
+  circle: require("../assets/circle.jpg"),
+  formidablelogo: require("../assets/formidable-logo.svg")
+};
+
+preloader(images);
+
+const theme = createTheme({
+  primary: "#857D41",
+  secondary: "#540804"
+});
+
+export default class Presentation extends React.Component {
+  render() {
+    return (
+      <Spectacle theme={theme}>
+        <Deck transition={["zoom", "slide"]} transitionDuration={500}>
+          <Slide transition={["zoom"]} bgColor="primary">
+            <Heading size={1} fit caps lineHeight={1} textColor="black">
+              Videodromm
+            </Heading>
+            <Heading size={1} fit caps>
+              Open source project
+            </Heading>
+            <Text textSize="1.5em" margin="20px 0px 0px" bold>By Bruce LANE</Text>
+          </Slide>
+        </Deck>
+      </Spectacle>
+    );
+  }
+}
